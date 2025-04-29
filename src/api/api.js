e6e75476077b56b867c1b5fc371be6f355f2d996
Sajid Khan <softwareengineersajid@gmail.com>
2025-04-30 01:00:58 +0500

    add backend url

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 import { Navigate } from 'react-router-dom';
 
-const API_BASE_URL = import.meta.env.VITE_API_UR || 'http://192.168.18.123:3000/api';
+const API_BASE_URL = "https://brunei-backend.vercel.app/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
